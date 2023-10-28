1f4db849e8e15fed2eacf9d6f2d2a4ac0e215ea9
Caio <108128119+DocCaio@users.noreply.github.com>
2023-10-28 09:32:40 -0300

    up

--- app/cards.js
+++ app/cards.js
@@ -1,2 +1,13 @@
-
+function escreverTexto() {
+    if (indice < texto.length) {
+      elementoMaquinaEscrever.textContent += texto.charAt(indice);
+      indice++;
+      setTimeout(escreverTexto, 100); // Ajuste a velocidade da digitação aqui (em milissegundos)
+    }
+  }
+  
+  // Iniciar a animação quando a página carregar
+  window.addEventListener("load", animarFlutuacao);
+  // Iniciar a animação quando a página carregar
+  window.addEventListener("load", escreverTexto);
 
