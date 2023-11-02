9ec6885300dcddec5d3ff35a937c478fe032a729
Caio <108128119+DocCaio@users.noreply.github.com>
2023-11-02 12:59:10 -0300

    up

--- app/darkTheme.js
+++ app/darkTheme.js
@@ -1,7 +1,33 @@
 const changeThemeBtn = document.querySelector("#change-theme");
 
-changeThemeBtn.addEventListener ("change" , function() {
-  
-  document.body.classList.toggle("dark");
 
-});
+// Togle dark mode
+function toggleDarkMode() {
+    document.body.classList.toggle("dark");
+}
+
+//Light or dark mode 
+
+function loadTheme() {
+    const darkMode = localStorage.getItem("dark");
+
+if(darkMode) {
+    toggleDarkMode();
+}
+
+}
+
+loadTheme();
+
+changeThemeBtn.addEventListener("change", function () {
+    toggleDarkMode();
+
+    // Save or remove dark mode
+    localStorage.removeItem("dark");
+
+
+    if(document.body.classList.contains("dark")) {
+        localStorage.setItem("dark , 1");
+    }
+});           
+       
