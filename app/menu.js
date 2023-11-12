0839cd0f15df4e90668eff44f0c0319f0c10226e
Caio <108128119+DocCaio@users.noreply.github.com>
2023-11-12 09:23:59 -0300

    up

---
+++ app/menu.js
@@ -0,0 +1,16 @@
+const botao = document.querySelector('.menu__button')
+botao.addEventListener('click', funcaoCallback)
+
+function funcaoCallback() {
+    const menuNav = document.querySelector('.menu__nav')
+    menuNav.classList.toggle('nav-active')
+  }
+
+  function funcaoCallback() {
+    const menuNav = document.querySelector('.menu__nav')
+    menuNav.classList.toggle('nav-active')
+  
+    button.textContent === "Abrir"
+      ? button.textContent = "Fechar"
+      : button.textContent = "Abrir"
+  }
