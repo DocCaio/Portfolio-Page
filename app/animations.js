d282a84d1ee2eac6ea352ac39a4df21d6ace97a0
Caio <108128119+DocCaio@users.noreply.github.com>
2023-11-21 08:45:36 -0300

    up

--- app/animations.js
+++ app/animations.js
@@ -1,35 +1,13 @@
-const flying = document.querySelector("#flying-img");
-const texto = "Halo! Saya Gunawan.";
-const elementoMaquinaEscrever = document.querySelector(".typewriter");
-
-function imgFlying() {
-    let distance = 20;
-    let duration = 2000;
-
+function typeWrite(elemento){
+  const textoArray = elemento.innerHTML.split('');
+  elemento.innerHTML = ' ';
+  textoArray.forEach(function(letra, i){   
     
-    imgFlying.style.transition = `transform ${duration}ms ease-in-out`;
-    imgFlying.style.transform = `translateY(-${distance}px)`;
+  setTimeout(function(){
+      elemento.innerHTML += letra;
+  }, 250 * i)
 
-  setTimeout(() => {
-    imgFlying.style.transition = `transform ${duration}ms ease-in-out`;
-    imgFlying.style.transform = `translateY(${distance}px)`;
-
-    setTimeout(() => {
-      animarFlutuacao();
-    }, duration);
-  }, duration);
+});
 }
-
-let indice = 0
-
-function escreverTexto() {
-  if (indice < texto.length) {
-    elementoMaquinaEscrever.textContent += texto.charAt(indice);
-    indice++;
-    setTimeout(escreverTexto, 100); // Ajuste a velocidade da digitação aqui (em milissegundos)
-  }
-}
-
-
-window.addEventListener("load", animarFlutuacao);
-window.addEventListener("load", escreverTexto);
+const titulo = document.querySelector('.bold-text');
+typeWrite(titulo);
