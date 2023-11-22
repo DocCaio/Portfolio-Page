0c8f7537f8b29d55fce25e041d9c7ffa1e8d9bc0
Caio <108128119+DocCaio@users.noreply.github.com>
2023-11-22 08:38:43 -0300

    up

--- app/menu.js
+++ app/menu.js
@@ -1,10 +1,2 @@
-const botao = document.querySelector('.menu__button')
-botao.addEventListener('click', funcaoCallback)
-
-function funcaoCallback() {
-    const menuNav = document.querySelector('.menu__nav')
-    menuNav.classList.toggle('nav-active')
-  
-}
 
  
