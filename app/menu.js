27e8b609086f51a3f6843b46284aa9497b00700d
Caio <108128119+DocCaio@users.noreply.github.com>
2023-11-22 13:51:13 -0300

    up

--- app/menu.js
+++ app/menu.js
@@ -1,2 +1,11 @@
+let menuButton = document.getElementsByClassName("menu-button");
+let menu = document.getElementsByClassName("mobile-menu-icon");
 
+menuButton.addEventListener("click", function() {
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+});
  
